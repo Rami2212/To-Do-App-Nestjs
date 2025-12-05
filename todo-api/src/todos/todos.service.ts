2a7aa87d8fc6193d9f09efd86970229dbf927482
Rami2212 <ramithapathmilarp@gmail.com>
2025-12-05 17:16:10 +0530

    change directory

---
+++ todo-api/src/todos/todos.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Not, Repository } from 'typeorm';
+import { Todo } from './entities/todo.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
+
+@Injectable()
+export class TodosService {
+    constructor(
+        @InjectRepository(Todo)
+        private readonly todoRepository: Repository<Todo>,
+    ) { }
+
+    // Get todos
+    async findAll(): Promise<Todo[]> {
+        return this.todoRepository.find({
+            order: { createdAt: 'DESC' },
+        });
+    }
+
+    // Create todo
+    async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+        const todo = this.todoRepository.create({
+            title: createTodoDto.title,
+            isCompleted: false,
+        });
+        return this.todoRepository.save(todo);
+    }
+
+    // Toggle todo completion
+    async toggle(id: string): Promise<Todo> {
+        const todo = await this.todoRepository.findOne({ where: { id } });
+        if (!todo) {
+            throw new NotFoundException('Todo not found');
+        }
+        todo.isCompleted = !todo.isCompleted;
+        return this.todoRepository.save(todo);
+    }
+}
