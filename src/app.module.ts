1fd7a8ce8ed4ce3e662774004a1a7d7df1638898
Rami2212 <ramithapathmilarp@gmail.com>
2025-12-05 16:15:39 +0530

    add db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TodosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // TypeORM DB connection
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'todo_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TodosModule,
+  ],
 })
 export class AppModule {}
