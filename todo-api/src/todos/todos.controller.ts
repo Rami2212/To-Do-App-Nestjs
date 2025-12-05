9873602f82e1d5303b7a6ca28ce76b16aed8bf69
Rami2212 <ramithapathmilarp@gmail.com>
2025-12-05 17:22:27 +0530

    add controller

--- todo-api/src/todos/todos.controller.ts
+++ todo-api/src/todos/todos.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Controller,
+    Get,
+    Post,
+    Patch,
+    Delete,
+    Body,
+    Param,
+    HttpCode,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { Todo } from './entities/todo.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+    constructor(private readonly todosService: TodosService) {}
+
+    // Get todos
+    @Get()
+    async findAll(): Promise<Todo[]> {
+        return this.todosService.findAll();
+    }
+
+    // Create todo
+    @Post()
+    async create(@Body() createTodoDto: CreateTodoDto): Promise<Todo> {
+        return this.todosService.create(createTodoDto);
+    }
+
+    // Toggle todo completion
+    @Patch(':id/toggle')
+    async toggle(@Param('id') id: string): Promise<Todo> {
+        return this.todosService.toggle(id);
+    }
+
+    // Delete todo
+    @Delete(':id')
+    @HttpCode(204)
+    async delete(@Param('id') id: string): Promise<void> {
+        return this.todosService.delete(id);
+    }
+}
